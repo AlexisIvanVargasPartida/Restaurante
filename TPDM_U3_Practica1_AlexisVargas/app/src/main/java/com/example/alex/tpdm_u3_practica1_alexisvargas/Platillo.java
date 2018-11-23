2d6e6f8abd2a1f5a0e195711aad5f4b2bf356311
AlexisIvanVargasPartida <alivvargaspa@ittepic.edu.mx>
2018-11-22 22:45:25 -0700

    Proyecto unidad 3

---
+++ TPDM_U3_Practica1_AlexisVargas/app/src/main/java/com/example/alex/tpdm_u3_practica1_alexisvargas/Platillo.java
@@ -0,0 +1,29 @@
+package com.example.alex.tpdm_u3_practica1_alexisvargas;
+
+public class Platillo {
+    String nombre;
+    int precio;
+    public Platillo(){
+
+    }
+    public Platillo(String nombre,int precio ){
+        this.nombre=nombre;
+        this.precio=precio;
+    }
+
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getPrecio(){
+        return precio;
+    }
+
+    public void setNombre(String nombre){
+        this.nombre=nombre;
+    }
+
+    public void setPrecio(int precio){
+        this.precio=precio;
+    }
+}
